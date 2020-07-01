e7c5af5c563727fd02a7b4005bbd200c6130003a
Teddy Bradsher <teddybradsher@gmail.com>
2020-07-01 12:14:08 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,5 +1,8 @@
 # Write your methods here
-def loop_message_five_times
-5.times do
-puts "message"
+def loop_message_five_times(message)
+  count = 0
+  while count < 5 do
+    puts message
+    count += 1
+  end
 end
